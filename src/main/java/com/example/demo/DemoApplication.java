c86bf69df0c53d65b438912253e0ce674fc80671
varshithakjayappa <varshitha.k.jayappa@gmail.com>
2025-02-03 18:20:51 +0100

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,12 +2,23 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
-public class DemoApplication {
+@Controller
+public class StartApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+    @GetMapping("/")
+    public String index(final Model model) {
+        model.addAttribute("title", "This is a SpringBoot Static Web Application");
+        model.addAttribute("msg", "Application Is Deployed To Kuberneets");
+        return "index";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(StartApplication.class, args);
+    }
 
 }
